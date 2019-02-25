c46444eee467d85b9b5d9c47562e0725bd7b9b66
Yosuke Sakamura <y-sakamura@digital-sphere.co.jp>
2019-02-25 18:00:54 +0900

    first commit

---
+++ d3Utils/connector.js
@@ -0,0 +1,123 @@
+const http = require('https')
+const querystring = require('querystring')
+
+function Connector() {
+  this.authData = null
+  this.tenant = null
+  this.sessionID = null
+}
+
+/**
+ * AUTHORIZATION 
+ * TODO : sessionID取ってくるとこもう少し頭良い感じにする
+ */
+Connector.prototype.auth = (uid, upw, tenant) => {
+
+  const self = this
+
+  self.authData = querystring.stringify({
+    'uid': uid,
+    'upw': upw
+  })
+  self.tenant = tenant
+  
+  const authOptions = {
+    hostname: 'd3w.ap.oproarts.com',
+    path: `/d3w/api/${self.tenant}/login`,
+    port: 443,
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Content-Length': Buffer.byteLength(self.authData)
+    }
+  }
+  
+  return new Promise((resolve,reject) => {
+    const authreq = http.request(authOptions, res => {
+      console.log(`STATUS: ${res.statusCode}`)
+      res.setEncoding('utf8')
+      const headers = JSON.stringify(res.headers).split(',')
+      headers.forEach((e,i) => {
+        if (i == 7) {
+          let sessionID = headers[i].slice(26,-31)
+          console.log('SESSIONID : ' + sessionID)
+          console.log('ログインしました。')
+          self.sessionID = sessionID
+          resolve()
+        }
+      })
+    })
+    authreq.on('error', e => {
+      console.error(`problem with request: ${e}`)
+      reject()
+    })
+    authreq.write(self.authData)
+    authreq.end()
+  })
+}
+
+/**
+ * ワーク一覧取得
+ */
+Connector.prototype.getWork = () => {
+  
+  const self = this
+  
+  const options = {
+    hostname: 'd3w.ap.oproarts.com',
+    path: `/d3w/api/${self.tenant}/conf/works`,
+    port: 443,
+    method: 'POST',
+    headers: {
+      'Cookie': self.sessionID
+    }
+  }
+  console.log(options)
+  const req = http.request(options, res => {
+    console.log(`STATUS: ${res.statusCode}`)
+    console.log(`HEADERS: ${JSON.stringify(res.headers)}`)
+    res.setEncoding('utf8')
+    res.on('data', (chunk) => {
+      console.log(`BODY: ${chunk}`)
+    })
+    res.on('end', () => {console.log('No more data')})
+  })
+  req.on('error', err => {
+    console.error(`ERROR: ${err}`)
+  })
+  req.end()
+}
+
+/**
+ * LOGOUT
+ */
+Connector.prototype.logout = () => {
+
+  const self = this
+  
+  const options = {
+    hostname: 'd3w.ap.oproarts.com',
+    path: `/d3w/api/${self.tenant}/logout`,
+    port: 443,
+    method: 'POST',
+    headers: {
+      'Cookie': self.sessionID
+    }
+  }
+
+  return new Promise((resolve,reject) => {
+    const request = http.request(options, res => {
+      console.log(`STATUS: ${res.statusCode}`)
+      console.log('ログアウトしました。')
+      resolve()
+    })
+    request.on('error', e => {
+      console.error(`problem with request: ${e}`)
+      console.log('ログアウト失敗')
+      reject();
+    })
+    request.end()
+  })
+}
+
+exports.Connector = Connector
---
+++ main.js
@@ -0,0 +1,12 @@
+const Connector = require('./d3Utils/connector').Connector
+
+/**
+ * Authorize
+ */
+const uid = 'xxx'
+const upw = 'yyy'
+const tenant = 'zzz'
+
+const conn = new Connector()
+conn.auth(uid, upw, tenant)
+.then(() => conn.getWork())
